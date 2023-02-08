221bb3f964ef4857bf139f44697c550f66323624
Jakub Filipek <39913230+Kubixii@users.noreply.github.com>
2023-02-08 16:51:56 +0100

    updated index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Content />
     </Router>
   </React.StrictMode>,
